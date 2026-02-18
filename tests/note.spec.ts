e934f66a2f2766a03eb931fc799c42238a262956
anjana881 <anzanabtr2016@gmail.com>
2026-02-18 18:39:33 +0545

    added parallel execution for tests

--- tests/note.spec.ts
+++ tests/note.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { NotePage } from '../pages/NotePage';
 
-test.describe('Note Functionality', () => {
+test.describe.parallel('Note Functionality', () => {
      
   test.beforeEach('should log in successfully with valid credentials', async ({ page }) => {
     const loginPage = new LoginPage(page);
--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -3,13 +3,11 @@ import { test, expect } from '@playwright/test';
 import { SignupPage } from '../pages/SignupPage';
 
 
-test.describe('Signup Functionality', () => {
+test.describe.parallel('Signup Functionality', () => {
   test('should signup successfully with valid credentials', async ({ page }) => {
     const signupPage = new SignupPage(page);
     await signupPage.goto();
     await signupPage.signup();
     
-    // const dashboardPage = new DashboardPage(page);
-    // await expect(dashboardPage.userProfile).toBeVisible();
   });
 });
